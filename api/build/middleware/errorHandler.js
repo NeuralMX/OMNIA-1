a89bd7107ce7d60dffaadef96f30830a9afc2166
Brandon G. Neri <arcaniteamp@gmail.com>
2023-03-27 01:14:45 -0600

    chore(): fix ignore build, recover README.md

--- api/build/middleware/errorHandler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorHandler = void 0;
-const errorHandler = (err, req, res, next) => {
-    const status = err.status || 500;
-    const message = err.message || "Error interno del servidor";
-    res.status(status).json({ message });
-};
-exports.errorHandler = errorHandler;
