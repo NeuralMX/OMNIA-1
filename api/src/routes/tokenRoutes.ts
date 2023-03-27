7e50f575adacb3bac99439d7740b29f6c07ba81b
Brandon G. Neri <arcaniteamp@gmail.com>
2023-03-27 01:00:49 -0600

    chore(): setup inicial

--- api/src/routes/tokenRoutes.ts
+++ api/src/routes/tokenRoutes.ts
@@ -1,11 +1,11 @@
-import express from "express";
-import { PrismaClient } from "@prisma/client";
-import { authorize } from "../middleware/auth";
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
+import { authorize } from '../middleware/auth';
 
 const prisma = new PrismaClient();
 const router = express.Router();
 
-router.get("/", authorize("admin"), async (_req, res) => {
+router.get('/', authorize('admin'), async (_req, res) => {
   try {
     const tokens = await prisma.token.findMany();
     res.json(tokens);
--- api/src/utils/authUtils.ts
+++ api/src/utils/authUtils.ts
@@ -1,6 +1,6 @@
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
-import { PrismaClient, TokenRole } from "@prisma/client";
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import { PrismaClient, TokenRole } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
@@ -8,31 +8,26 @@ export const createToken = async (hash: string): Promise<string> => {
   const token = await prisma.token.findUnique({ where: { hash } });
 
   if (!token) {
-    throw new Error("Token no encontrado.");
+    throw new Error('Token no encontrado.');
   }
 
-  const jwtToken = jwt.sign(
-    { id: token.id, email: token.email, role: token.role },
-    process.env.JWT_SECRET || "",
-    { expiresIn: "1h" }
-  );
+  const jwtToken = jwt.sign({ id: token.id, email: token.email, role: token.role }, process.env.JWT_SECRET || '', {
+    expiresIn: '1h',
+  });
 
   return jwtToken;
 };
 
 export const verifyToken = (jwtToken: string): Record<string, unknown> => {
   try {
-    const decoded = jwt.verify(jwtToken, process.env.JWT_SECRET || "");
+    const decoded = jwt.verify(jwtToken, process.env.JWT_SECRET || '');
     return decoded as Record<string, unknown>;
   } catch (error) {
-    throw new Error("Token inválido.");
+    throw new Error('Token inválido.');
   }
 };
 
-export const generateHash = async (
-  email: string,
-  role: TokenRole
-): Promise<string> => {
+export const generateHash = async (email: string, role: TokenRole): Promise<string> => {
   const salt = await bcrypt.genSalt(10);
   const hash = await bcrypt.hash(`${email}-${role}-${Date.now()}`, salt);
   return hash;
