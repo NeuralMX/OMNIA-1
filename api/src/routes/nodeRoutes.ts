93f06215fb5c0bba1311629493f45971f267e1e4
Brandon G. Neri <arcaniteamp@gmail.com>
2023-03-27 18:06:47 -0600

    feat(OMN-9): added detail to Node, Task and misc changes

--- api/src/routes/nodeRoutes.ts
+++ api/src/routes/nodeRoutes.ts
@@ -14,12 +14,107 @@ router.get('/', authenticate, authorize('admin'), async (_req, res) => {
   }
 });
 
-router.post('/', authenticate, authorize('admin'), async (req, res) => {
-  const { name, ipAddress } = req.body;
+router.get('/:id', authenticate, authorize('admin'), async (req, res) => {
+  const { id } = req.params;
 
   try {
-    const node = await prisma.node.create({ data: { name, ipAddress } });
+    const node = await prisma.node.findUnique({ where: { id } });
+    if (!node) {
+      res.status(404).json({ message: 'Node not found' });
+    } else {
+      res.json(node);
+    }
+  } catch (error) {
+    res.status(500).json({ message: (error as Error).message });
+  }
+});
+
+router.post('/', authenticate, authorize('admin'), async (req, res) => {
+  const {
+    name,
+    ipAddress,
+    description,
+    gpuModel,
+    gpuMemory,
+    gpuTemperature,
+    cpuModel,
+    cpuCores,
+    cpuSpeed,
+    ram,
+    storage,
+    os,
+  } = req.body;
+
+  try {
+    const node = await prisma.node.create({
+      data: {
+        name,
+        ipAddress,
+        description,
+        gpuModel,
+        gpuMemory,
+        gpuTemperature,
+        cpuModel,
+        cpuCores,
+        cpuSpeed,
+        ram,
+        storage,
+        os,
+      },
+    });
     res.json(node);
+  } catch (error) {
+    res.status(500).json({ message: (error as Error).message });
+  }
+});
+
+router.put('/:id', authenticate, authorize('admin'), async (req, res) => {
+  const { id } = req.params;
+  const {
+    name,
+    ipAddress,
+    description,
+    gpuModel,
+    gpuMemory,
+    gpuTemperature,
+    cpuModel,
+    cpuCores,
+    cpuSpeed,
+    ram,
+    storage,
+    os,
+  } = req.body;
+
+  try {
+    const node = await prisma.node.update({
+      where: { id },
+      data: {
+        name,
+        ipAddress,
+        description,
+        gpuModel,
+        gpuMemory,
+        gpuTemperature,
+        cpuModel,
+        cpuCores,
+        cpuSpeed,
+        ram,
+        storage,
+        os,
+      },
+    });
+    res.json(node);
+  } catch (error) {
+    res.status(500).json({ message: (error as Error).message });
+  }
+});
+
+router.delete('/:id', authenticate, authorize('admin'), async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await prisma.node.delete({ where: { id } });
+    res.json({ message: 'Node deleted' });
   } catch (error) {
     res.status(500).json({ message: (error as Error).message });
   }
--- api/src/seed.ts
+++ api/src/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, TaskStatus, TokenRole } from "@prisma/client";
+import { PrismaClient, TaskStatus, TaskType, TokenRole } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
@@ -8,51 +8,87 @@ async function main() {
       email: 'omnia-1@neuralmx.com',
       hash: 'QUITAN_EL_TRAPO_Y_NO_LO_PONEN',
       role: TokenRole.admin,
-    }
-  })
+    },
+  });
   console.log('Master Token creado', masterToken);
-  
+
   const node1 = await prisma.node.create({
     data: {
-      name: "Nodo 1",
-      ipAddress: "192.168.0.1",
-      description: "Este es el nodo 1 de la red.",
-      tasks: {
-        create: [
-          {
-            description: "Tarea 1 del nodo 1",
-            status: TaskStatus.pending,
-          },
-          {
-            description: "Tarea 2 del nodo 1",
-            status: TaskStatus.completed,
-          },
-        ],
-      },
+      name: 'Nodo 1',
+      ipAddress: '192.168.0.1',
+      description: 'Este es el nodo 1 de la red.',
+      gpuModel: 'NVIDIA GeForce RTX 3080',
+      gpuMemory: 10,
+      gpuTemperature: 75.5,
+      cpuModel: 'Intel Core i7-11700K',
+      cpuCores: 8,
+      cpuSpeed: 3.6,
+      ram: 16,
+      storage: 512,
+      os: 'Windows 10 Pro',
     },
+  });
+
+  await prisma.task.createMany({
+    data: [
+      {
+        nodeId: node1.id,
+        description: 'Tarea 1 del nodo 1',
+        type: TaskType.inference,
+        code: 'print("Hola mundo")',
+        payload: JSON.stringify({ image_url: 'https://mi_imagen.jpg' }),
+        status: TaskStatus.pending,
+      },
+      {
+        nodeId: node1.id,
+        description: 'Tarea 2 del nodo 1',
+        type: TaskType.training,
+        code: 'print("Entrenando modelo...")',
+        payload: JSON.stringify({ dataset_url: 'https://mi_dataset.csv' }),
+        status: TaskStatus.completed,
+      },
+    ],
   });
 
   const node2 = await prisma.node.create({
     data: {
-      name: "Nodo 2",
-      ipAddress: "192.168.0.2",
-      description: "Este es el nodo 2 de la red.",
-      tasks: {
-        create: [
-          {
-            description: "Tarea 1 del nodo 2",
-            status: TaskStatus.in_progress,
-          },
-          {
-            description: "Tarea 2 del nodo 2",
-            status: TaskStatus.pending,
-          },
-        ],
-      },
+      name: 'Nodo 2',
+      ipAddress: '192.168.0.2',
+      description: 'Este es el nodo 2 de la red.',
+      gpuModel: 'NVIDIA GeForce RTX 3070',
+      gpuMemory: 8,
+      gpuTemperature: 70.2,
+      cpuModel: 'AMD Ryzen 9 5900X',
+      cpuCores: 12,
+      cpuSpeed: 3.7,
+      ram: 32,
+      storage: 1024,
+      os: 'Ubuntu 20.04 LTS',
     },
   });
 
-  console.log("Datos de prueba creados con éxito.", node1, node2);
+  await prisma.task.createMany({
+    data: [
+      {
+        nodeId: node2.id,
+        description: 'Tarea 1 del nodo 2',
+        type: TaskType.fine_tuning,
+        code: 'print("Fine-tuning de modelo...")',
+        payload: JSON.stringify({ model_url: 'https://mi_modelo.h5' }),
+        status: TaskStatus.in_progress,
+      },
+      {
+        nodeId: node2.id,
+        description: 'Tarea 2 del nodo 2',
+        type: TaskType.execute_code,
+        code: 'print("Ejecutando código personalizado...")',
+        payload: JSON.stringify({}),
+        status: TaskStatus.pending,
+      },
+    ],
+  });
+
+  console.log('Datos de prueba creados con éxito.', node1, node2);
 
   prisma.$disconnect();
 }
